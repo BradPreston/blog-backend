e059e90b00e34037b30ec1b1beeb76b819399043
bradpreston <bap5393@gmail.com>
2022-09-11 11:55:00 -0400

    setup handlers.go

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -1,1 +1,18 @@
 package app
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (s *Server) ApiStatus(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{
+		"status": "success",
+		"data":   "blog post API is running properly",
+	}
+
+	resJSON, _ := json.MarshalIndent(response, "", "    ")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resJSON)
+}
