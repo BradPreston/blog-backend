0c816830090943ce215669400bae540de5f0e896
bradpreston <bap5393@gmail.com>
2022-09-11 11:25:15 -0400

    setup server.go

--- pkg/app/server.go
+++ pkg/app/server.go
@@ -1,1 +1,33 @@
 package app
+
+import (
+	"log"
+
+	"github.com/BradPreston/blog-backend/pkg/api"
+	"github.com/go-chi/chi"
+)
+
+type Server struct {
+	router      *chi.Router
+	postService api.PostService
+}
+
+func NewServer(router *chi.Router, postService api.PostService) *Server {
+	return &Server{
+		router:      router,
+		postService: postService,
+	}
+}
+
+func (s *Server) Run() error {
+	r := s.Routes()
+
+	err := r.Run()
+
+	if err != nil {
+		log.Printf("server - there was an error calling Run on router: %v", err)
+		return err
+	}
+
+	return nil
+}
