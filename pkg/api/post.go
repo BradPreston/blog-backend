6e668b3a23d92ecd7a22ffa2768280fd33da8faf
bradpreston <bap5393@gmail.com>
2022-09-11 11:15:55 -0400

    setup post.go

--- pkg/api/post.go
+++ pkg/api/post.go
@@ -1,1 +1,15 @@
 package api
+
+type PostService interface{}
+
+type PostRepository interface{}
+
+type postService struct {
+	storage PostRepository
+}
+
+func NewPostService(postRepo PostRepository) PostService {
+	return &postService{
+		storage: postRepo,
+	}
+}
