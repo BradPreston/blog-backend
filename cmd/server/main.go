394454bca81882ada62b3dedd69dd737a0b2d142
bradpreston <bap5393@gmail.com>
2022-09-11 11:03:53 -0400

    setup main.go file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,71 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"os"
 
+	"github.com/BradPreston/blog-backend/pkg/api"
+	"github.com/BradPreston/blog-backend/pkg/app"
+	"github.com/BradPreston/blog-backend/pkg/repository"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "startup error: %s\\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	env, err := godotenv.Read(".env")
+	if err != nil {
+		return err
+	}
+
+	connectionString := env["PSQL_URI"]
+
+	db, err := setupDatabase(connectionString)
+	if err != nil {
+		return err
+	}
+
+	storage := repository.NewStorage(db)
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	postService := api.NewPostService(storage)
+
+	server := app.NewServer(router, postService)
+
+	err = server.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setupDatabase(connString string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/BradPreston/blog-backend
 
 go 1.18
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
