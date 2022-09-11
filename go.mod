03e2f3834c005e737735d6d82651a86820bd7a76
bradpreston <bap5393@gmail.com>
2022-09-11 11:14:13 -0400

    setup storage.go

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-chi/chi v1.5.4 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
 )
--- pkg/repository/storage.go
+++ pkg/repository/storage.go
@@ -1,1 +1,15 @@
 package repository
+
+import "database/sql"
+
+type Storage interface{}
+
+type storage struct {
+	db *sql.DB
+}
+
+func NewStorage(db *sql.DB) Storage {
+	return &storage{
+		db: db,
+	}
+}
